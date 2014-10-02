3a1ce7ae0f27601e1d3e48cd5184932312538e90
Alesom <alesom.zorzi@gmail.com>
2014-10-02 11:37:13 -0300

    pasta Final

---
+++ COM_ESC2011/Comunicacao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO.Ports;
+namespace ConsoleApplication1
+{
+    class Comunicacao
+    {
+        public Comunicacao()
+        {
+            SerialCom.PortName = "COM8";
+            SerialCom.BaudRate=19200;
+            SerialCom.Parity = 0;
+            SerialCom.DataBits = 8;
+            SerialCom.StopBits = (StopBits) 1;
+
+        //    SerialComPort.ReadTimeout = 500;
+            
+        }
+
+        private SerialPort SerialCom = new SerialPort();
+
+        public void Comunica()
+        {
+            try
+            {
+                SerialCom.Open();
+            }
+            catch { Console.WriteLine("************Error!!!**********"); }
+        }
+    }
+        
+}
