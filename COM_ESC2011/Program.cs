728bb46ac731ffc3f742ff887fb6dfd8f641974c
Alesom <alesom.zorzi@gmail.com>
2014-10-02 07:45:59 -0300

    Testes iniciais

---
+++ COM_ESC2011/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nNome do Cliente:");
+            string nome = Console.ReadLine();
+            Console.WriteLine(String.Format("\nO cliente {0} foi cadastrado com sucesso", nome));
+            Console.ReadLine();
+        }
+    }
+}
