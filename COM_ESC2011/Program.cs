2f2df61050bda3d55500e261b6a819d9d4dfe1a9
Alesom <alesom.zorzi@gmail.com>
2014-10-02 11:35:25 -0300

    Conectando, mas sem receber resposta.

--- COM_ESC2011/Program.cs
+++ COM_ESC2011/Program.cs
@@ -2,26 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.IO.Ports;
 
 namespace ConsoleApplication1
 {
-    public bool isOpen
+    static class Program
     {
-        get
+        static void Main()
         {
-                return SerialComPort.IsOpen;
-        }
-    }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("\nNome do Cliente:");
-            string nome = Console.ReadLine();
-            Console.WriteLine(String.Format("\nO cliente {0} foi cadastrado com sucesso", nome));
-            Console.ReadLine();
-        }
+            Comunicacao C = new Comunicacao();
+        } 
     }
 }
