5faaefe509a1662b579a76da87e9fd6d8e2a9449
Alesom <alesom.zorzi@gmail.com>
2014-10-03 08:40:43 -0300

    Comunicando e recebendo Resposta!

--- COM_ESC2011/Comunicacao.cs
+++ COM_ESC2011/Comunicacao.cs
@@ -7,28 +7,64 @@ namespace ConsoleApplication1
 {
     class Comunicacao
     {
+        private SerialPort SerialCom = new SerialPort();
+
+        private string Buffer = string.Empty;
+
         public Comunicacao()
         {
-            SerialCom.PortName = "COM8";
+            SerialCom.PortName = "COM7";
             SerialCom.BaudRate=19200;
             SerialCom.Parity = 0;
             SerialCom.DataBits = 8;
             SerialCom.StopBits = (StopBits) 1;
 
         //    SerialComPort.ReadTimeout = 500;
-            
+            SerialCom.DataReceived += new SerialDataReceivedEventHandler(SerialCom_DataReceived);
         }
 
-        private SerialPort SerialCom = new SerialPort();
+        void SerialCom_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //throw new NotImplementedException();
+            Buffer = SerialCom.ReadExisting();
+            Console.WriteLine(Buffer);
+        }
 
-        public void Comunica()
+
+        public void ComunicacaoOpen()
         {
             try
             {
                 SerialCom.Open();
             }
             catch { Console.WriteLine("************Error!!!**********"); }
+            if (SerialCom.IsOpen)
+            {
+                Console.WriteLine("Conexão!");
+            }
+            else
+            {
+                Console.WriteLine("Sem conexão!");
+            }
         }
+
+        public void TransmiteDado(string Dado)
+        {
+            if (SerialCom.IsOpen)
+            {
+                SerialCom.Write(Dado);
+            }
+            else
+            {
+                Console.WriteLine("A porta esta fechada!");
+            }
+        }
+
+        public void FechaComunicacao()
+        {
+            SerialCom.Close();
+        }
+
     }
         
 }
--- COM_ESC2011/Program.cs
+++ COM_ESC2011/Program.cs
@@ -10,6 +10,14 @@ namespace ConsoleApplication1
         static void Main()
         {
             Comunicacao C = new Comunicacao();
-        } 
+            C.ComunicacaoOpen();
+            while (true)
+            {
+                C.TransmiteDado (Console.ReadLine()+"\r\n");
+            }
+            C.TransmiteDado("+++\r\n");
+            Console.ReadLine();
+            C.FechaComunicacao();
+        }
     }
 }
