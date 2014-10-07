be39cc219fca7b3201ed84d1b19b819ce842b4b4
Alesom <alesom.zorzi@gmail.com>
2014-10-07 10:11:26 -0300

    Criando as pastas no diretório

---
+++ COM_ESC2011/AgrupaDados.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApplication1
+{
+    class AgrupaDados
+    {
+        public AgrupaDados()
+        {
+            Principal = string.Empty;
+            Full = false;
+        }
+
+        private string Principal;
+        private Boolean Full; //indica se a string esta completa, ou seja, se tem \n\r
+
+
+        private Boolean getisFull()
+        {
+            return Full;
+        }
+
+        private void setisFull(Boolean NFull)
+        {
+           Full=NFull;
+        }
+
+        public Boolean Central(string Word)
+        {
+            if (this.getisFull())
+                return false;
+            else
+            {
+                for (int i = 0; i < Word.Length; i++)
+                {
+                    char A = Word[i];
+                    if (A == '\n' || A == '\r')
+                    {
+                        this.ConcatenaStrings(Word);
+                        Console.WriteLine("oi");
+                        setisFull(false);
+                        return false;
+                    }
+                    
+                }
+
+                this.ConcatenaStrings(Word);
+                return true;
+            }
+        }
+
+        public string getPrincipal()
+        {
+            return Principal;
+        }
+
+        public void setPrincipal(string neWord)
+        {
+            Principal=neWord;
+        }
+
+        private void ConcatenaStrings(string soma)
+        {
+            Principal+=soma;
+        }
+
+
+    }
+}
---
+++ COM_ESC2011/GerenciaDiretorio.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApplication1
+{
+    class GerenciaDiretorio
+    {
+        private string Path;
+        private string Nome;
+
+        public GerenciaDiretorio() //construtor;
+        {
+            Path = string.Empty;
+            Nome = string.Empty;
+        }
+        
+        public string getNome(){ //get nome da pasta
+            return this.Nome;
+        }
+
+        public void setPath(string caminho) //set path da pasta
+        {
+            this.Path = caminho;
+        }
+
+        public void setNome(string caminho) //set nome da pasta
+        {
+            this.Nome = caminho;
+        }
+
+        public string getPath() //get path da pasta
+        {
+            return this.Path;
+        }
+
+        public void Cria() //cria o diretório
+        {
+            System.IO.Directory.CreateDirectory(System.IO.Path.Combine(Path, Nome));
+        }
+
+        public Boolean DirJaExist() //verifica se o diretório já existe
+        {
+            if (System.IO.File.Exists(System.IO.Path.Combine(Path, Nome)))
+                return true;
+
+            return false;
+        }
+    }
+}
